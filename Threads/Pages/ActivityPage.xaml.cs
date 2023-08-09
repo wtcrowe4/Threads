6d6a6e45585da781cf1ee0f30dfc7679de966d15
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-09 17:51:18 -0400

    Activity page

---
+++ Threads/Pages/ActivityPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Threads.Pages
+{
+    public partial class ActivityPage : ContentPage
+    {
+        public ActivityPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
