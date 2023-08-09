ec9fb0c2d4d47c10a9aeb4a5ec2de704232e37c9
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-09 11:22:15 -0400

    added icon, started on home page

---
+++ Threads/Pages/HomePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Threads.Helpers;
+using Threads.Controls;
+
+
+namespace Threads.Pages;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+    }
+
+    protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
+    {
+        base.OnNavigatedFrom(args);
+    }
+
+}
+
