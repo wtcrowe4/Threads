56b0c6290f28fa61c8756d039fbddf4945266f7e
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-12 09:28:46 -0400

    created threadview/userview

---
+++ Threads/Controls/ThreadView.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Maui;
+using Threads.Helpers;
+using Threads.Models;
+using CommunityToolkit.Mvvm;
+
+namespace Threads.Controls;
+
+public class ThreadView 
+{ 
+
+
+}
+
---
+++ Threads/Controls/UserView.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Maui;
+using Threads.Helpers;
+using Threads.Models;
+using CommunityToolkit.Mvvm;
+
+namespace Threads.Controls;
+
+ public class UserView
+{
+
+
+}
