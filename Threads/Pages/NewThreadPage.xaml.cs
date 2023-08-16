0c5b94cc005ac5a863ffb88af9779c42ace16f40
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-16 17:30:10 -0400

    Starting on NewThreadPage

--- Threads/Pages/NewThreadPage.xaml.cs
+++ Threads/Pages/NewThreadPage.xaml.cs
@@ -1,10 +1,40 @@
-﻿namespace Threads.Pages
+﻿using System.Windows.Input;
+using Threads.Models;
+
+namespace Threads.Pages
 {
     public partial class NewThreadPage : ContentPage
     {
+        private readonly User currentUser;
         public NewThreadPage()
         {
             InitializeComponent();
+            currentUser = GetCurrentUser();
+            CurrentUserImage.Source = currentUser.Image;
+            CurrentUserName.Text = currentUser.UserName;
+
+            
+
         }
+
+        private static User GetCurrentUser()
+        {
+            return new User()
+            {
+                UserName = "User 1",
+                DisplayName = "John Smith",
+                Followers = 100,
+                Image = "default_user.png",
+                IsVerified = true,
+                IsFollowing = true,
+                HasSimiliarFollowers = true
+
+            };
+        }
+
+        public ICommand GoBackCommand => new Command(async () => await Navigation.PopAsync());
     }
+       
+
+    
 }
