c1aab3aa96a413b9a79470dd1ee381d49174d03d
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-19 07:03:43 -0400

    Activity model

--- Threads/Models/Activity.cs
+++ Threads/Models/Activity.cs
@@ -8,5 +8,12 @@ namespace Threads.Models
 {
     public class Activity
     {
+        public string Icon { get; set; }
+        public string Action { get; set; }
+        public string Message { get; set; }
+        public Thread Thread { get; set; }
+        public User UserAct { get; set; }
+        public User UserRec { get; set; }
+        public string TimeAgo { get; set; }
     }
 }
