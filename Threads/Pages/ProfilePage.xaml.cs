358adee319952e83acaec6d879768ba9565ada71
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-24 06:28:17 -0400

    Added current user data

--- Threads/Pages/ProfilePage.xaml.cs
+++ Threads/Pages/ProfilePage.xaml.cs
@@ -1,10 +1,40 @@
-﻿namespace Threads.Pages
+﻿using Threads.Models;
+
+namespace Threads.Pages
 {
+
     public partial class ProfilePage : ContentPage
     {
+        private User _currentUser;
         public ProfilePage()
         {
             InitializeComponent();
+            _currentUser = GetCurrentUser();
+            CurrentUserDisplay.Text = _currentUser.DisplayName;
+            CurrentUserImage.Source = _currentUser.Image;
+            CurrentUserUN.Text = _currentUser.UserName;
+            FollowersDisplay.Text = _currentUser.FollowersDisplay;
+
         }
+
+
+        private static User GetCurrentUser()
+        {
+            return new User
+            {
+                UserName = "ME864",
+                DisplayName = "John Smith",
+                Followers = 100,
+                Image = "default_user.png",
+                IsVerified = true,
+                IsFollowing = false,
+                HasSimiliarFollowers = true
+            };
+        }
+
     }
+
 }
+
+
+
