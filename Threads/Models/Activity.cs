5834c77eca0948a5fd42d0d65e0a04128e3e4f98
Thomas Crowe <wtcrowe4@outlook.com>
2023-09-04 17:06:04 -0400

    Profile page list view, Activiy enum

--- Threads/Models/Activity.cs
+++ Threads/Models/Activity.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Threads.Models;
 
 namespace Threads.Models
 {
     public class Activity
     {
         public string Icon { get; set; }
-        public string Action { get; set; }
+        public enum ActionType
+        {
+            Reply,
+            Repost,
+            Follow,
+            Like,
+            Mention
+        }
+        public ActionType Action { get; set; } 
         public string Message { get; set; }
         public Thread Thread { get; set; }
         public User UserAct { get; set; }
--- Threads/Pages/ProfilePage.xaml.cs
+++ Threads/Pages/ProfilePage.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Diagnostics;
+﻿using CallKit;
+using CommunityToolkit.Maui.Converters;
+using System.Diagnostics;
 using System.Xml.Serialization;
 using Threads.Models;
+
+
+
 
 namespace Threads.Pages
 {
@@ -8,6 +13,8 @@ namespace Threads.Pages
     public partial class ProfilePage : ContentPage
     {
         private readonly User _currentUser;
+        private List<Models.Activity> _activity;
+        private List<Models.Thread> _threads;
         public ProfilePage()
         {
             InitializeComponent();
@@ -18,6 +25,7 @@ namespace Threads.Pages
             FollowersDisplay.Text = _currentUser.FollowersDisplay;
             FollowersImage.Source = _currentUser.Image;
             FollowersImage2.Source = _currentUser.Image;
+            ActivityLV.ItemsSource =  _threads;
         }
 
 
@@ -38,16 +46,60 @@ namespace Threads.Pages
         private void ThreadsSelected(object sender, EventArgs e)
         {
             Debug.WriteLine("Threads Selected");
+            ThreadsLabel.TextColor = Color.FromArgb("#1DA1F2");
+            UnderlineThreads.BackgroundColor = Color.FromArgb("#1DA1F2");
+
+            //Get Threads
+            //Dummy Data
+            _threads = new List<Models.Thread>
+             {
+                new Models.Thread
+                {
+                   User = _currentUser,
+                   Message = "This is a test message",
+                   Likes = 10,
+                   Replies = 5,
+                   TimeAgo = "1h"
+
+                },
+                new Models.Thread
+                {
+                   User = _currentUser,
+                   Message = "This is a test message",
+                   Likes = 10,
+                   Replies = 5,
+                   TimeAgo = "2h"
+
+                },
+                new Models.Thread
+                {
+                   User = _currentUser,
+                   Message = "This is a test message",
+                   Likes = 10,
+                   Replies = 5,
+                   TimeAgo = "4h"
+                }
+             };
         }
 
         private void RepliesSelected(object sender, EventArgs e)
         {
             Debug.WriteLine("Replies Selected");
+            RepliesLabel.TextColor = Color.FromArgb("#1DA1F2");
+            UnderlineReplies.BackgroundColor = Color.FromArgb("#1DA1F2");
+
+            //Get Replies
+
         }
 
         private void RepostsSelected(object sender, EventArgs e)
         {
             Debug.WriteLine("Reposts Selected");
+            RepostsLabel.TextColor = Color.FromArgb("#1DA1F2");
+            UnderlineReposts.BackgroundColor = Color.FromArgb("#1DA1F2");
+
+            //Get Reposts
+
         }
 
     }
