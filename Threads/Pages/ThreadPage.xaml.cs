8dd9386f36e7b76a61da683239c9aa6f387bf588
Thomas Crowe <wtcrowe4@outlook.com>
2023-11-30 22:32:51 -0500

    working on filler conent for page

--- Threads/Pages/ThreadPage.xaml.cs
+++ Threads/Pages/ThreadPage.xaml.cs
@@ -1,9 +1,70 @@
+using Threads.Models;
+using Thread = Threads.Models.Thread;
 namespace Threads.Pages;
+
 
 public partial class ThreadPage : ContentPage
 {
 	public ThreadPage()
 	{
 		InitializeComponent();
+		
+		ThreadList = new List<Activity>
+		{
+			new Activity
+			{
+				Action = Activity.ActionType.Reply,
+				Message = "This is a reply",
+				Thread = new Thread
+				{
+					Content = "This is a thread",
+					Author = new User
+					{
+						Username = "User1"
+
+					}
+				},
+				UserAct = new User { Username = "User2" },
+				UserRec = new User { Username = "User3" },
+				TimeAgo = "1h"
+				},
+			new Activity
+			{
+                Action = Activity.ActionType.Repost,
+                Message = "This is a repost",
+                Thread = new Thread
+				{
+                    Content = "This is a thread",
+                    Author = new User
+					{
+                        Username = "User1"
+
+                    }
+                },
+                UserAct = new User { Username = "User2" },
+                UserRec = new User { Username = "User3" },
+                TimeAgo = "1h"
+                },	
+			new Activity
+			{
+                Action = Activity.ActionType.Follow,
+                Message = "This is a follow",
+                Thread = new Thread
+				{
+                    Content = "This is a thread",
+                    Author = new User
+					{
+                        Username = "User1"
+
+                    }
+                },
+                UserAct = new User { Username = "User2" },
+                UserRec = new User { Username = "User3" },
+                TimeAgo = "1h"
+                },
+
 	}
+}
+
+    public List<Activity> ThreadList { get; }
 }
