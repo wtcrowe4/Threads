a4c1b03a0e2aa725d08bbaace7c7142c61c69796
Thomas Crowe <wtcrowe4@outlook.com>
2023-12-17 15:48:58 -0500

    time ago field to not throw errors on pages

--- Threads/Models/Thread.cs
+++ Threads/Models/Thread.cs
@@ -49,6 +49,8 @@ namespace Threads.Models
         public bool HasReplies => Replies > 0;
         public bool HasLikes => Likes > 0;
         public string LikesRepliesDisplay => $"{Replies} Replies ✯ {Likes} Likes";
+
+        public string TimeAgo { get; internal set; }
     }
 }
 
